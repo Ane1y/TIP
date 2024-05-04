8ad9b08de0ba41e969d6ecf00c566adb4ef6f496
Anelya Dusaeva <an.dusaeva@gmail.com>
2024-05-05 01:02:05 +0300

    added the sign analysis

--- src/tip/lattices/GenericLattices.scala
+++ src/tip/lattices/GenericLattices.scala
@@ -146,9 +146,9 @@ class PowersetLattice[A] extends Lattice {
 
   type Element = Set[A]
 
-  val bottom: Element = ??? //<--- Complete here
-
-  def lub(x: Element, y: Element): Element = ??? //<--- Complete here
+  val bottom: Element = Set()
+  // lower upper bound
+  def lub(x: Element, y: Element): Element = x union y
 }
 
 /**
